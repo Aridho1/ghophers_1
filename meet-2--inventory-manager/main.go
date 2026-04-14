6921eeb66375e95a9f9673281bd82472c272bb08
Aridho1 <muhamadaridho859@gmail.com>
2026-04-14 14:16:27 +0700

    feat: improve user interface and functionality in main.go by enhancing menu display, adding product listing, and refining input handling

--- meet-2--inventory-manager/main.go
+++ meet-2--inventory-manager/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"slices"
+	"strings"
+	"time"
 )
 
 type Product struct {
@@ -12,13 +16,21 @@ type Product struct {
 	Stock int
 }
 
-const AppName string = "====== Toko Kelontong | INVENTORY MANAGER ======"
+const AppName string = "====== [ Toko Kelontong | INVENTORY MANAGER ] ======"
+const line = "-----------------------"
+var reader = bufio.NewReader(os.Stdin)
 var menus = []string{"Tambah Barang Ke Gudang", "Lihat Semua Stock Barang", "Beli Barang", "Keluar"}
 var products = []Product{}
 
-func title() {
+func title(loopCount int) {
+
+	newLine := "\n\n"
+	if loopCount != 0 {
+		newLine = "\n\n\n\n"
+	}
 	
-	fmt.Print("\n\n",AppName, "\n\n")
+	fmt.Printf("%v%v\n\n", newLine, AppName)
+	// fmt.Printf("\n\n\n\n",AppName, "\n\n")
 	for index, menu := range menus {
 		fmt.Printf("%d. %v\n", index + 1, menu)
 	}
@@ -36,35 +48,49 @@ func title() {
 // 	}
 // }
 
+func getListProduct(title string) {
+    fmt.Printf("\n\n====== %s ======\n", title)
+
+	productsLen := len(products)
+	
+	for i, product := range products {
+
+		barrier := "---------------------------------"
+		if i + 1 == productsLen {
+			barrier = "================================="
+		}
+
+		fmt.Printf("ID: %v	| Nama	: %v	\n	| Harga	: %v	\n	| Stok	: %v\n%v\n", product.ID, product.Name, product.Price, product.Stock, barrier)
+		// fmt.Printf("ID: %v | Nama: %v=|=Harga: Rp %v | Stok: %v\n", product.ID, product.Name, product.Price, product.Stock)
+	}
+	
+	// fmt.Print("\n")
+}
+
 func task__() {
-	var inputItemName string
+	// var inputItemName string
 	var inputMenu, inputPrice, inputStock, inputID, inputCount, inputMoney int
 
 	menuLen := len(menus)
 	
-	for {
-		title()
+	for loopCount := 0; loopCount >= 0; loopCount++ {
+		title(loopCount)
 
-		fmt.Printf("\n\n\nPilih Menu [1-%d]: ", menuLen)
+		fmt.Printf("\nPilih Menu [1-%d]: ", menuLen)
 		fmt.Scanln(&inputMenu)
 
 		switch inputMenu {
 		case 1:
 			fmt.Printf("Masukan Nama Barang: ")
-			fmt.Scanln(&inputItemName)
-			
+			// fmt.Scanln(&inputItemName)
+			inputItemName, _ := reader.ReadString('\n')
+			inputItemName = strings.TrimSpace(inputItemName)
+				
 			fmt.Printf("Masukan Harga Barang: ")
 			fmt.Scanln(&inputPrice)
 			
 			fmt.Printf("Masukan Stock Barang: ")
 			fmt.Scanln(&inputStock)
-			
-			// product := Product{
-			// 	ID: 	len(products) + 1,
-			// 	Name: 	inputItemName,
-			// 	Price: 	price,
-			// 	Stock: 	stock,
-			// }
 
 			products = append(products, Product{
 				ID: 	len(products) + 1,
@@ -73,48 +99,63 @@ func task__() {
 				Stock: 	inputStock,
 			})
 
-			fmt.Printf("\n[SYSTEM]: Barang berhasil ditambahkan ke gudang.")
+			fmt.Printf("\n[SYSTEM]: Barang berhasil ditambahkan ke gudang.\n")
 		case 2:
-			fmt.Print("\n====== Daftar Stock Gudang ======\n")
-
-			for _, product := range products {
-				fmt.Printf("ID: %d	| Nama:	%v		| Harga: Rp %d		| Stok: %d", product.ID, product.Name, product.Price, product.Stock)
-			}
-
-			fmt.Printf("=====================\nTotal Jenis Barang: %d", len(products) + 1)
+			getListProduct("Daftar Stock Gudang")
+			fmt.Printf("\nTotal Jenis Barang: %d\n", len(products))
 		case 3:
-			fmt.Printf("Masukan ID Barang yang mau dibeli: ")
+			getListProduct("Daftar Produk")
+			fmt.Printf("\nMasukan ID Barang yang mau dibeli: ")
 			fmt.Scanln(&inputID)
 
 			productIndex := slices.IndexFunc(products, func(p Product) bool {
 				return p.ID == inputID
 			})
 			if productIndex == -1 {
-				fmt.Print("[SYSTEM]: Barang dengan ID tersebut tidak ditemukan.")
+				fmt.Print("[SYSTEM]: Barang dengan ID tersebut tidak ditemukan.\n")
 				continue
 			}
+
+			product := &products[productIndex]
 			
 			fmt.Printf("Masukan jumlah Barang yang mau dibeli: ")
 			fmt.Scanln(&inputCount)
+
+			price := inputCount * product.Price
 			
+			fmt.Printf("[SYSTEM]: Total Harga: Rp %d\n", price)
 			fmt.Printf("Masukan uang anda: ")
 			fmt.Scanln(&inputMoney)
 
+			changeMoney := inputMoney - price
+
+			if changeMoney < 0 {
+				fmt.Printf("[SYSTEM]: Uang anda kurang %d !!!\n", changeMoney * -1)
+				continue
+			}
+
+			product.Stock -= inputCount
+
+			fmt.Printf("\n[SYSTEM]: Transaksi berhasil!\n")
+			fmt.Printf("[SYSTEM]: Kembalian: %d\n", changeMoney)
+			fmt.Printf("[SYSTEM]: Stok %v sekarang: %d pcs\n", product.Name, product.Stock)
+			// fmt.Printf("%#+ lalu %#v\n\n", product, products)
+
 		case 4:
-			fmt.Print("[SYSTEM]: Program selesai. Selamat Beristirahat, Bos.")
+			if loopCount == 0 {
+				fmt.Printf("\n[SYSTEM]: 	Sedih... :(\n		Kamu bahkan belum melakukan aksi apapun :(\n\n")
+			} else {
+				fmt.Print("[SYSTEM]: Program selesai. Selamat Beristirahat, Bos.")
+			}
 			return
 		default:
 			// 
 		}
+
+		time.Sleep(2 * time.Second)
 	}
 }
 
 func main() {
-	// task__()
-	var num int
-
-	fmt.Print("input number: ")
-	fmt.Scanln(&num)
-
-	fmt.Printf("%d + 1 = %d", num, num + 1)
+	task__()
 }
