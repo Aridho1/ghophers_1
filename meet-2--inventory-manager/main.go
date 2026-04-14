de27fa929fa4658033037c19473637ac5bfc673f
Aridho1 <muhamadaridho859@gmail.com>
2026-04-14 09:40:20 +0700

    chore: update go.mod to require golang.org/x/sys v0.43.1

---
+++ meet-2--inventory-manager/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+	"strconv"
+)
+
+type Product struct {
+	ID    int
+	Name  string
+	Price int
+	Stock int
+}
+
+const AppName string = "====== Toko Kelontong | INVENTORY MANAGER ======"
+var menus = []string{"Tambah Barang Ke Gudang", "Lihat Semua Stock Barang", "Beli Barang", "Keluar"}
+var products = []Product{}
+
+func title() {
+	
+	fmt.Print("\n\n",AppName, "\n\n")
+	for index, menu := range menus {
+		fmt.Printf("%d. %v\n", index + 1, menu)
+	}
+}
+
+// func inputInt(q string, errMsg *string) {
+// 	var input, _input string
+
+// 	for {
+// 		fmt.Print(q)
+// 		fmt.Scanln(&_input)
+
+// 		input, err = strconv.Atoi(_input)
+
+// 	}
+// }
+
+func task__() {
+	var inputMenu, inputItemName, inputPrice, inputStock, inputID, inputCount, inputMoney string
+	menuLen := len(menus)
+	
+	for {
+		title()
+
+		fmt.Printf("\n\n\nPilih Menu [1-%d]: ", menuLen)
+		fmt.Scanln(&inputMenu)
+
+		switch inputMenu {
+		case "1":
+			fmt.Printf("Masukan Nama Barang: ")
+			fmt.Scanln(&inputItemName)
+			
+			fmt.Printf("Masukan Harga Barang: ")
+			fmt.Scanln(&inputPrice)
+			
+			fmt.Printf("Masukan Stock Barang: ")
+			fmt.Scanln(&inputStock)
+
+			price, err := strconv.Atoi(inputPrice)
+			if err != nil {
+				price = 0
+			}
+
+			stock, err := strconv.Atoi(inputStock)
+			if err != nil {
+				stock = 0
+			}
+			
+			// product := Product{
+			// 	ID: 	len(products) + 1,
+			// 	Name: 	inputItemName,
+			// 	Price: 	price,
+			// 	Stock: 	stock,
+			// }
+
+			products = append(products, Product{
+				ID: 	len(products) + 1,
+				Name: 	inputItemName,
+				Price: 	price,
+				Stock: 	stock,
+			})
+
+			fmt.Printf("\n[SYSTEM]: Barang berhasil ditambahkan ke gudang.")
+		case "2":
+			fmt.Print("\n====== Daftar Stock Gudang ======\n")
+
+			for _, product := range products {
+				fmt.Printf("ID: %d	| Nama:	%v		| Harga: Rp %d		| Stok: %d", product.ID, product.Name, product.Price, product.Stock)
+			}
+
+			fmt.Printf("=====================\nTotal Jenis Barang: %d", len(products) + 1)
+		case "3":
+			fmt.Printf("Masukan ID Barang yang mau dibeli: ")
+			fmt.Scanln(&inputID)
+
+			selectedID, err := strconv.Atoi(inputID)
+			if err != nil {
+				fmt.Print("[SYSTEM]: ID barang harus berupa angka.")
+				continue
+			}
+
+			productIndex := slices.IndexFunc(products, func(p Product) bool {
+				return p.ID == selectedID
+			})
+			if productIndex == -1 {
+				fmt.Print("[SYSTEM]: Barang dengan ID tersebut tidak ditemukan.")
+				continue
+			}
+			
+			fmt.Printf("Masukan jumlah Barang yang mau dibeli: ")
+			fmt.Scanln(&inputCount)
+			
+			fmt.Printf("Masukan uang anda: ")
+			fmt.Scanln(&inputMoney)
+
+		case "4":
+			fmt.Print("[SYSTEM]: Program selesai. Selamat Beristirahat, Bos.")
+			return
+		default:
+			// 
+		}
+	}
+}
+
+func main() {
+	// task__()
+	var num int
+
+	fmt.Print("input number: ")
+	fmt.Scanln(&num)
+
+	fmt.Printf("%d + 1 = %d", num, num + 1)
+}
